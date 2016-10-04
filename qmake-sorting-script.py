14583c0f5274d91082e5f6c23c3cca74882441c5
Robert Hildebrandt <robert-42@web.de>
2016-10-04 23:28:34 +0200

    refactoring

--- qmake-sorting-script.py
+++ qmake-sorting-script.py
@@ -1,5 +1,11 @@
 #!/bin/env python
 
+def resort_file(filename):
+    with open(filename, 'rU') as file:
+        for line in file:
+            print(line.replace('\n', ''))
+
+
 from argparse import ArgumentParser
 
 #https://docs.python.org/2/library/argparse.html
@@ -8,6 +14,4 @@ parser.add_argument('--files', dest='files', metavar='FILES', type=str, nargs='+
 args = parser.parse_args()
 
 for filename in args.files:
-    with open(filename, 'rU') as file:
-        for line in file:
-            print(line.replace('\n', ''))
+    resort_file(filename)
