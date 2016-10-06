458d3f32bca56e88ebf4fe6a1cd59ae88b5edd18
Robert Hildebrandt <robert-42@web.de>
2016-10-06 09:12:01 +0200

    started implementing test script

---
+++ test/run-tests.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+DIR=$(pwd)
+TEST_DIR=$DIR/test-files
+
+rm -rf $TEST_DIR
+mkdir $TEST_DIR
+
+cd $TEST_DIR
+
+echo '
+SOURCES += bbb ccc aaa
+CONFIG= x y z
+' > test1.pro
+
+echo '
+SOURCES+=\
+bbb ccc aaa
+CONFIG= x y z
+' > test2.pro
+
+echo '
+SOURCES+=\
+bbb\
+ccc \
+aaa
+CONFIG= x y z
+' > test3.pro
+
+echo '
+SOURCES+=bbb \
+ccc \
+aaa
+CONFIG= x y z
+' > test4.pro
+
+echo '
+SOURCES+=ddd aaa \
+bbb fff \
+eee ccc
+CONFIG= x y z
+' > test5.pro
+
+echo '
+SOURCES+=bbb \
+\
+ccc \
+\
+aaa \
+\
+
+CONFIG= x y z
+' > test6.pro
+
+
