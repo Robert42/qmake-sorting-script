ca700c58fed7a75b32f277fcd5464866e1fd6a71
Robert Hildebrandt <robert-42@web.de>
2016-10-06 09:24:43 +0200

    executing now the script

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,24 +2,26 @@
 
 DIR=$(dirname $(readlink -f $0))
 TEST_DIR=$DIR/test-files
+SCRIPT=$DIR/../qmake-sorting-script.py
+
 
 rm -rf $TEST_DIR
 mkdir $TEST_DIR
 
 cd $TEST_DIR
 
-echo '
+echo '# handle all in the first line
 SOURCES += bbb ccc aaa
 CONFIG= x y z
 ' > test1.pro
 
-echo '
+echo '# handle all in the second line
 SOURCES+=\
 bbb ccc aaa
 CONFIG= x y z
 ' > test2.pro
 
-echo '
+echo '# handle first line empty
 SOURCES+=\
 bbb\
 ccc \
@@ -27,21 +29,21 @@ aaa
 CONFIG= x y z
 ' > test3.pro
 
-echo '
+echo '# handle first line non empty
 SOURCES+=bbb \
 ccc \
 aaa
 CONFIG= x y z
 ' > test4.pro
 
-echo '
+echo '# handle multiple per line
 SOURCES+=ddd aaa \
 bbb fff \
 eee ccc
 CONFIG= x y z
 ' > test5.pro
 
-echo '
+echo '# handle empty following lines
 SOURCES+=bbb \
 \
 ccc \
@@ -52,4 +54,7 @@ aaa \
 CONFIG= x y z
 ' > test6.pro
 
+RUN_SCRIPT=$SCRIPT --files test1.pro test2.pro test3.pro test4.pro test5.pro test6.pro
+
+$RUN_SCRIPT
 
